9fd054654554db2e6d4fd58a161d3fb2d7caeb09
Ivan Bilan <iwantegren@gmail.com>
2024-06-07 17:17:49 +0300

    add interceptor

--- src/interceptors/response-status.interceptor.ts
+++ src/interceptors/response-status.interceptor.ts
@@ -4,7 +4,7 @@ import {
   Injectable,
   NestInterceptor,
 } from '@nestjs/common';
-import { catchError, Observable, tap, throwError } from 'rxjs';
+import { map, Observable } from 'rxjs';
 
 @Injectable()
 export class StatusResponseInterceptor implements NestInterceptor {
@@ -12,21 +12,6 @@ export class StatusResponseInterceptor implements NestInterceptor {
     context: ExecutionContext,
     next: CallHandler<any>,
   ): Observable<any> | Promise<Observable<any>> {
-    const now = Date.now();
-    console.log('intercept');
-    return next.handle().pipe(
-      tap((data) => {
-        const response = context.switchToHttp().getResponse();
-        console.log(
-          `Response... Status: ${response.statusCode}, Time: ${Date.now() - now}ms, Data: ${JSON.stringify(data)}`,
-        );
-      }),
-      catchError((err) => {
-        console.log(
-          `Error... Status: ${err.status || 500}, Time: ${Date.now() - now}ms, Error: ${JSON.stringify(err.message)}`,
-        );
-        return throwError(err);
-      }),
-    );
+    return next.handle().pipe(map((data) => ({ ...data, success: true })));
   }
 }
