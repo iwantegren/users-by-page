3f9371f930dbdfcec2e307fbe5d5ade32612498a
Ivan Bilan <iwantegren@gmail.com>
2024-07-15 23:07:32 +0300

    remove log

--- src/decorators/url.decorator.ts
+++ src/decorators/url.decorator.ts
@@ -4,7 +4,6 @@ export const ReqUrl = createParamDecorator(
   (data: unknown, ctx: ExecutionContext): string => {
     const request = ctx.switchToHttp().getRequest();
     const protocol = request.headers['x-forwarded-proto'] || request.protocol;
-    console.log({ reqUrl_protocol: protocol });
     return `${protocol}://${request.get('host')}${request.baseUrl}${request.path}`;
   },
 );
@@ -13,7 +12,6 @@ export const HostUrl = createParamDecorator(
   (data: unknown, ctx: ExecutionContext): string => {
     const request = ctx.switchToHttp().getRequest();
     const protocol = request.headers['x-forwarded-proto'] || request.protocol;
-    console.log({ hostUrl_protocol: protocol });
     return `${protocol}://${request.get('host')}`;
   },
 );
